50eefe6e1e00fc31499099e485e875f5bb83c241
Seow Alex <seowalex@hotmail.com>
2021-09-13 23:25:49 +0800

    Persist token

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,18 +1,49 @@
-import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
+import {
+  Action,
+  ThunkAction,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import api from '../api/base';
 /* eslint-disable import/no-cycle */
 import auth from '../reducers/authSlice';
 /* eslint-enable import/no-cycle */
 
-export const store = configureStore({
-  reducer: {
-    [api.reducerPath]: api.reducer,
-    auth,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: [api.reducerPath],
+};
+
+const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
+  auth,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(api.middleware),
+});
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import App from './App';
-import { store } from './app/store';
+import { persistor, store } from './app/store';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
