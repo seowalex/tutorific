f727ff20e21bd5ca4ec8be3f45d0ac5ce54adfa0
Seow Alex <seowalex@hotmail.com>
2021-09-22 22:22:52 +0800

    Fix chat link when logged out

--- frontend/src/components/ChatRouterLink.tsx
+++ frontend/src/components/ChatRouterLink.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useIonRouter, useIonToast } from '@ionic/react';
 
+import { useAppSelector } from '../app/hooks';
+import { selectCurrentUserId } from '../reducers/auth';
 import { useAddChatMutation, useGetChatsQuery } from '../api/chat';
 
 interface Props {
@@ -10,24 +12,31 @@ interface Props {
 const ChatRouterLink: React.FC<Props> = ({ children, profileId }) => {
   const [addChat] = useAddChatMutation();
   const { data: chats } = useGetChatsQuery();
+  const currentUserId = useAppSelector(selectCurrentUserId);
 
   const router = useIonRouter();
   const [present] = useIonToast();
 
   const handleChat = async () => {
-    let chatId = chats?.find((chat) => chat.otherProfile.id === profileId)?.id;
+    if (currentUserId) {
+      let chatId = chats?.find(
+        (chat) => chat.otherProfile.id === profileId
+      )?.id;
 
-    if (chatId) {
-      router.push(`/chats/${chatId}`);
-    } else if (window.navigator.onLine) {
-      chatId = await addChat(profileId).unwrap();
-      router.push(`/chats/${chatId}`);
+      if (chatId) {
+        router.push(`/chats/${chatId}`);
+      } else if (window.navigator.onLine) {
+        chatId = await addChat(profileId).unwrap();
+        router.push(`/chats/${chatId}`);
+      } else {
+        present({
+          message: 'No internet connection',
+          color: 'danger',
+          duration: 2000,
+        });
+      }
     } else {
-      present({
-        message: 'No internet connection',
-        color: 'danger',
-        duration: 2000,
-      });
+      router.push('/login');
     }
   };
 
