126a97e4475bbb8380e99bdd352e83f7565a5e2b
shadowezz <wenjie.cao@hotmail.com>
2021-09-23 12:51:49 +0800

    Check duplicate subscription.

--- backend/src/controllers/subscription.ts
+++ backend/src/controllers/subscription.ts
@@ -6,6 +6,20 @@ const addSubscription = async (ctx: Koa.Context): Promise<void> => {
   const subscriptionJson = ctx.request.body;
   const { profileId } = ctx.state.user;
 
+  const existingSubscription =
+    await subscriptionService.getExisitingSubscription(
+      profileId,
+      JSON.stringify(subscriptionJson)
+    );
+
+  if (existingSubscription !== undefined) {
+    existingSubscription.profile = profileId;
+    ctx.body = {
+      data: existingSubscription,
+    };
+    return;
+  }
+
   const newSubscription = await subscriptionService.createSubscription({
     profile: profileId,
     subscriptionJson: JSON.stringify(subscriptionJson),
