24576764d64e3d05a5391a5338206ed3bea1df7a
shadowezz <wenjie.cao@hotmail.com>
2021-09-24 16:55:24 +0800

    Remove get and delete subscription endpoints.

--- backend/src/routes/subscription.ts
+++ backend/src/routes/subscription.ts
@@ -4,7 +4,5 @@ import subscriptionController from '../controllers/subscription';
 const router: Router = new Router();
 
 router.post('/', subscriptionController.addSubscription);
-router.delete('/', subscriptionController.deleteSubscription);
-router.get('/', subscriptionController.getSubscriptions);
 
 export default router;
